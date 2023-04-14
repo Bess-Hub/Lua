bb1a4133ef77a2dab17200f0319805a639728c13
Bessem Hamud <118807215+Bess-Hub@users.noreply.github.com>
2023-04-14 21:48:43 +0200

    Create Fundamentals.lua

---
+++ Fundamentals.lua
@@ -0,0 +1,12 @@
+<!-- Let's take a look at a programming language developed by my _paisano_. We are talking about LUA! -->
+<!-- Easier than Python, faster than many languages. Interacts great with C codes and it is great to embed in other applications like WoW and Roblox -->
+<!-- So if you are like me and love games, get over here! -->
+
+<!-- Create a variable -->
+local var1 = "Hello Earth!"
+<!-- Haha! Earth because LUA... :) Now seriously, declaring the variable local is an option, if not declared it will be considered global -->
+<!-- What does this mean? That I will see tomorrow and update this file -->
+
+<!-- For today, let's just leave the option to print it, just like in Python -->
+print(var1)
+
