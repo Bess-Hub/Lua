36dbe15ce1c032eeb5d4d4ceb07b9bf901089a60
Bessem Hamud <118807215+Bess-Hub@users.noreply.github.com>
2023-04-17 19:08:13 +0200

    Update Fundamentals.lua

--- Fundamentals.lua
+++ Fundamentals.lua
@@ -1,15 +1,15 @@
-<!-- Let's take a look at a programming language developed by my _paisano_. We are talking about LUA! -->
-<!-- Easier than Python, faster than many languages. Interacts great with C codes and it is great to embed in other applications like WoW and Roblox -->
-<!-- So if you are like me and love games, get over here! -->
+-- Let's take a look at a programming language developed by my _paisano_. We are talking about LUA! --
+-- Easier than Python, faster than many languages. Interacts great with C codes and it is great to embed in other applications like WoW and Roblox --
+-- So if you are like me and love games, get over here! --
 
-<!-- Create a variable -->
+-- Create a variable --
 local var1 = "Hello Earth!"
 local var2 = 384,400
-<!-- Haha! Earth because LUA... :) Now seriously, declaring the variable local is an option, if not declared it will be considered global -->
-<!-- What does this mean? That I will see tomorrow and update this file -->
+-- Haha! Earth because LUA... :) Now seriously, declaring the variable local is an option, if not declared it will be considered global --
+-- What does this mean? That I will see tomorrow and update this file --
 
-<!-- For today, let's just leave the option to print it, just like in Python -->
+-- For today, let's just leave the option to print it, just like in Python --
 print(var1)
 
-<!-- Or we could print it like this: -->
+-- Or we could print it like this: --
 io.write(var1, " We are ", var2, " thousand kilometers away!", "\n", " That is a big ", type(var2), "!")
