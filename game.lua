47d8d606212f9c58d5ad01ec6ce019f78a683ed0
Bessem Hamud <118807215+Bess-Hub@users.noreply.github.com>
2023-05-19 22:13:27 +0200

    Update game.lua

--- game.lua
+++ game.lua
@@ -1,42 +1,87 @@
+-- Time variables
+local startTime = os.time()
+
 -- Room definitions
 local rooms = {
   {
-    name = "Living Room",
-    description = "You are in a cozy living room. There is a door to the north.",
+    name = "Class Room",
+    description = "There is a door to the north.",
     north = 2,
-    objects = { "painting" }
-  },
-  {
-    name = "Kitchen",
-    description = "You are in a bright kitchen. There is a door to the south.",
-    south = 1,
-    objects = { "knife" }
+    objects = { "blackboard", "clock" , "backpack", "desk", "chair", "fan", "jacket"}
   }
 }
 
 -- Object definitions
 local objects = {
-  painting = {
-    description = "A beautiful landscape painting.",
-    examine = "You notice a small safe behind the painting."
+  blackboard = {
+    description = "A regular blackboard... Is that chalk?",
+    examine = "Someone drew an arrow pointing up. What could be there?"
   },
-  knife = {
-    description = "A sharp kitchen knife.",
-    examine = "It looks like it hasn't been used in a while."
+  clock = {
+    description = "A clock on top of the blackboard.",
+    examine = "The clock is broken. But you can't see the time, keep on looking you might find a clue."
+  },
+  backpack = {
+    description = "Your backpack",
+    examine = "Here is a weird letter: 'The code of the door is the time the clock is frozen at.'"
+  },
+  desk = {
+    description = "Your desk",
+    examine = ""
+  },
+  chair = {
+    description = "Your compi's chair",
+    examine = "Ew, there is bubblegum on it. Not touching that, nothing to see here."
+  },
+   fan = {
+    description = "Could there be anything on this fan?",
+    examine = "Shit... I cannot reach it."
+  },
+  jacket = {
+    description = "This jacket is not yours...",
+    examine = "There is something in the pocket! It is a piece of paper that reads: 'A mother has 30 dollars to split between her two daughters, what time is it?"
   }
 }
 
 -- Player position
 local currentRoom = 1
 
+-- Hiding something here unsuspiciously
+local doorCode = "1345"
+
+-- Flag to track initial text display
+local initialTextDisplayed = false
+
+-- Display initial welcome text
+print()
+print("Welcome to our first Bely Escape Game!")
+print("You must be very bored to be playing this game.")
+print("Unlike your current class, this one you can actually escape.")
+print()
+print("Quick instructions:")
+print("1. You woke up and class is over. Your colleagues have left the classroom. It seems that you are locked in there.")
+print("2. You will have to explore the objects around to find clues that lead you out of that place.")
+print("3. To examine an object simply type 'Examine' followed by the object name. Example: Examine backpack.")
+print("4. To try opening the door and leaving your class type 'Open door'.")
+print("5. Easy? Let's see how long it takes for you to complete it...")
+print()
+
 -- Game loop
 while true do
   -- Display current room
   print(rooms[currentRoom].name)
-  print(rooms[currentRoom].description)
+  
+  -- Display initial text if not displayed yet
+  if not initialTextDisplayed and currentRoom == 1 then
+    print("You are in the class room.")
+    initialTextDisplayed = true
+  else
+    print(rooms[currentRoom].description)
+  end
   
   -- Display objects in the room
   if #rooms[currentRoom].objects > 0 then
+    print()
     print("You see the following objects:")
     for _, objectName in ipairs(rooms[currentRoom].objects) do
       print("- " .. objectName)
@@ -44,28 +89,50 @@ while true do
   end
   
   -- Prompt for player input
+  print()
   print("What would you like to do?")
   local input = io.read()
   
-  -- Process player input
-  if input == "north" and rooms[currentRoom].north then
-    currentRoom = rooms[currentRoom].north
-  elseif input == "south" and rooms[currentRoom].south then
-    currentRoom = rooms[currentRoom].south
-  elseif input == "quit" then
-    break  -- Exit the game loop
-  elseif input:sub(1, 7) == "examine" then
-    local objectName = input:sub(9)  -- Extract the object name from input
-    local object = objects[objectName]
-    if object then
-      print(object.description)
+ -- Process player input
+if input:lower() == "open door" then
+  if currentRoom == 1 and rooms[currentRoom].north then
+    print()
+    print("Enter the door code:")
+    local codeInput = io.read()
+    if codeInput == doorCode then
+      currentRoom = rooms[currentRoom].north
+      local endTime = os.time()
+      local elapsedTime = os.difftime(endTime, startTime)
+      print()
+      print("You did it! You left your class and you promise never to sleep during it again! Congratulations!")
+      print("Time taken: " .. elapsedTime .. " seconds.")
+      print()
+      break -- Exit the game loop
     else
-      print("You don't see that object here.")
+      print("Wrong code! The door remains locked.")
     end
   else
-    print("Invalid input!")
+    print("You can't open the door here.")
   end
+elseif input:lower() == "leave" and rooms[currentRoom].south then
+  currentRoom = rooms[currentRoom].south
+elseif input:lower() == "quit" then
+  break  -- Exit the game loop
+elseif input:sub(1, 7):lower() == "examine" then
+  print()
+  local objectName = input:sub(9)  -- Extract the object name from input
+  local object = objects[objectName]
+  if object then
+    print(object.description)
+    print(object.examine)
+    print()
+  else
+    print("You don't see that object here.")
+  end
+else
+  print("Invalid input!")
 end
 
-print("Thanks for playing!")
+print()
+end
 
