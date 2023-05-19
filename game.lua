3ddb46366c982e70bbcc24727c9d75a57b7076c6
Bessem Hamud <118807215+Bess-Hub@users.noreply.github.com>
2023-05-19 20:52:19 +0200

    Update game.lua

--- game.lua
+++ game.lua
@@ -3,12 +3,26 @@ local rooms = {
   {
     name = "Living Room",
     description = "You are in a cozy living room. There is a door to the north.",
-    north = 2
+    north = 2,
+    objects = { "painting" }
   },
   {
     name = "Kitchen",
     description = "You are in a bright kitchen. There is a door to the south.",
-    south = 1
+    south = 1,
+    objects = { "knife" }
+  }
+}
+
+-- Object definitions
+local objects = {
+  painting = {
+    description = "A beautiful landscape painting.",
+    examine = "You notice a small safe behind the painting."
+  },
+  knife = {
+    description = "A sharp kitchen knife.",
+    examine = "It looks like it hasn't been used in a while."
   }
 }
 
@@ -21,8 +35,16 @@ while true do
   print(rooms[currentRoom].name)
   print(rooms[currentRoom].description)
   
+  -- Display objects in the room
+  if #rooms[currentRoom].objects > 0 then
+    print("You see the following objects:")
+    for _, objectName in ipairs(rooms[currentRoom].objects) do
+      print("- " .. objectName)
+    end
+  end
+  
   -- Prompt for player input
-  print("Where would you like to go?")
+  print("What would you like to do?")
   local input = io.read()
   
   -- Process player input
@@ -32,9 +54,18 @@ while true do
     currentRoom = rooms[currentRoom].south
   elseif input == "quit" then
     break  -- Exit the game loop
+  elseif input:sub(1, 7) == "examine" then
+    local objectName = input:sub(9)  -- Extract the object name from input
+    local object = objects[objectName]
+    if object then
+      print(object.description)
+    else
+      print("You don't see that object here.")
+    end
   else
     print("Invalid input!")
   end
 end
 
 print("Thanks for playing!")
+
